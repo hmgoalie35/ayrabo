34b3d9e530bd968d29643bfcbcbb11572620ff66
Harris Pittinsky <hpittin1@binghamton.edu>
2016-06-26 23:50:15 -0400

    fixes #13

--- apps/divisions/models.py
+++ apps/divisions/models.py
@@ -3,11 +3,14 @@ from leagues.models import League
 
 
 class Division(models.Model):
-    name = models.CharField(max_length=128, unique=True, verbose_name='Name')
+    name = models.CharField(max_length=128, verbose_name='Name')
     league = models.ForeignKey(League)
 
     class Meta:
         ordering = ['name']
+        unique_together = (
+            ('name', 'league'),
+        )
 
     def __str__(self):
         return self.name
