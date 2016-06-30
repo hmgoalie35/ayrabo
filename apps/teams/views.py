d17ff55628a7f8af26ff497acf551411474ca96e
Harris Pittinsky <hpittin1@binghamton.edu>
2016-06-30 00:01:39 -0400

    finish teams feature. Added ability to bulk upload teams from csv for staff

--- apps/teams/views.py
+++ apps/teams/views.py
@@ -1,3 +1,87 @@
-from django.shortcuts import render
+import csv
+from io import TextIOWrapper
 
-# Staff only page to bulk upload from csv
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.urlresolvers import reverse_lazy
+from django.db.utils import IntegrityError
+from django.shortcuts import redirect, render
+from django.views.generic import FormView
+from django.contrib import messages
+
+from divisions.models import Division
+from .forms import BulkUploadTeamsForm
+from .models import Team
+
+
+class BulkUploadTeamsView(LoginRequiredMixin, FormView):
+    form_class = BulkUploadTeamsForm
+    template_name = 'teams/bulk_upload_teams.html'
+    success_url = reverse_lazy('bulk_upload_teams')
+
+    def get(self, *args, **kwargs):
+        if not self.request.user.is_staff:
+            return redirect(reverse_lazy('home'))
+        return super(BulkUploadTeamsView, self).get(*args, **kwargs)
+
+    def post(self, *args, **kwargs):
+        if not self.request.user.is_staff:
+            return redirect(reverse_lazy('home'))
+        return super(BulkUploadTeamsView, self).post(*args, **kwargs)
+
+    def form_valid(self, form):
+        uploaded_file = form.cleaned_data.get('file')
+        errors, successful_teams_created, total_csv_rows = self.parse_csv(uploaded_file)
+        if len(errors) > 0:
+            context = self.get_context_data()
+            context['errors'] = errors
+            return render(self.request, 'teams/bulk_upload_teams.html', context)
+
+        messages.success(self.request, '{successful_teams_created} out of {total_csv_rows} teams successfully created'
+                         .format(successful_teams_created=successful_teams_created, total_csv_rows=total_csv_rows-1))
+        return super(BulkUploadTeamsView, self).form_valid(form)
+
+    def parse_csv(self, uploaded_file):
+        # uploaded_file is a bytesio object, but DictReader needs stringio, so convert it
+        csv_file = TextIOWrapper(uploaded_file.file)
+        reader = csv.DictReader(csv_file)
+        errors = []
+        successful_teams_created = 0
+        line_no = 1
+        for row in reader:
+            # These variables will be None if the corresponding header does not exist in the file
+            team_name = row.get('Team Name', None)
+            website = row.get('Website', None)
+            division = row.get('Division', None)
+
+            # Check to make sure the headings were specified in the .csv file
+            if team_name is None or website is None or division is None:
+                errors.append(
+                        'You must include Team Name, Website and Division as headings in the .csv '
+                        'on line {lineno}'.format(lineno=line_no))
+                return errors, successful_teams_created, line_no
+
+            # Make sure the value of the headers aren't empty strings
+            if team_name.strip() == '' or division.strip() == '':
+                errors.append("Team Name and/or Division can't be blank on line {lineno}".format(lineno=line_no))
+                return errors, successful_teams_created, line_no
+
+            # Attempt to locate the division object the user wants to create a team under
+            try:
+                division_obj = Division.objects.get(name=division)
+            except Division.DoesNotExist:
+                errors.append(
+                        'The division {division} does not currently exist, you need to create it '
+                        'under the correct league and sport'.format(division=division))
+                return errors, successful_teams_created, line_no
+
+            # Attempt to create a team, ignoring duplicates.
+            try:
+                Team.objects.create(name=team_name, website=website, division=division_obj)
+                successful_teams_created += 1
+            except IntegrityError:
+                # Ignore duplicates
+                successful_teams_created += 1
+
+            line_no += 1
+
+        return errors, successful_teams_created, line_no
