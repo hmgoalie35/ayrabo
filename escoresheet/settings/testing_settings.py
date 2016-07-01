0cd0128716059a76b36038ba8a707b332eb3076a
Harris Pittinsky <hpittin1@binghamton.edu>
2016-07-01 17:53:52 -0400

    resolves #25

--- escoresheet/settings/testing_settings.py
+++ escoresheet/settings/testing_settings.py
@@ -1,9 +1,30 @@
-# This file is inherting settings from local settings, see the last lines of settings.py
 import logging
+
+from django.conf import settings
 
 # Django automatically sets DEBUG = False when running tests
 
 logging.disable(logging.CRITICAL)
+
+CSRF_COOKIE_SECURE = False
+SESSION_COOKIE_SECURE = False
+SECURE_SSL_REDIRECT = False
+SECURE_PROXY_SSL_HEADER = None
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(settings.BASE_DIR, 'test_db.sqlite3'),
+    }
+}
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+    }
+}
 
 PASSWORD_HASHERS = [
     'django.contrib.auth.hashers.MD5PasswordHasher',
