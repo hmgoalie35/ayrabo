9300b06407aabbb35df0a417ed34152c8096f7ea
Harris Pittinsky <hpittin1@binghamton.edu>
2016-07-01 17:37:29 -0400

    move abbreviated name generation to .clean method, resolves issue #22

--- apps/leagues/models.py
+++ apps/leagues/models.py
@@ -1,21 +1,22 @@
 from django.db import models
+
 from sports.models import Sport
 
 
 class League(models.Model):
-    full_name = models.CharField(max_length=256, unique=True, verbose_name='Full Name',
+    full_name = models.CharField(max_length=256, verbose_name='Full Name',
                                  error_messages={'unique': 'League with this name already exists'})
     abbreviated_name = models.CharField(max_length=32, verbose_name='Abbreviated Name')
     sport = models.ForeignKey(Sport, verbose_name='Sport')
 
     class Meta:
         ordering = ['full_name']
+        unique_together = (('full_name', 'sport'),)
 
-    def save(self, *args, **kwargs):
+    def clean(self):
         # Takes the first letter of each word and concatenates them together. ex: National Hockey League --> NHL
         words = self.full_name.split(' ')
         self.abbreviated_name = ''.join([word[:1].strip().upper() for word in words])
-        super(League, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.full_name
--- apps/leagues/tests/test_models.py
+++ apps/leagues/tests/test_models.py
@@ -1,38 +1,51 @@
-from django.test import TestCase
-from .factories.LeagueFactory import LeagueFactory
-from leagues.models import League
 from django.db.utils import IntegrityError
+from django.test import TestCase
+
 from escoresheet.testing_utils import is_queryset_in_alphabetical_order
+from leagues.models import League
+from sports.tests.factories.SportFactory import SportFactory
+from .factories.LeagueFactory import LeagueFactory
 
 
 class LeagueModelTests(TestCase):
+    def setUp(self):
+        self.ice_hockey = SportFactory.create(name='Ice Hockey')
 
     def test_default_ordering(self):
         LeagueFactory.create_batch(5)
         self.assertTrue(is_queryset_in_alphabetical_order(League.objects.all(), 'full_name'))
 
     def test_to_string(self):
-        nhl = LeagueFactory.create(full_name='National Hockey League')
+        nhl = LeagueFactory.create(full_name='National Hockey League', sport=self.ice_hockey)
         self.assertEqual(str(nhl), 'National Hockey League')
 
-    def test_unique_full_name(self):
-        LeagueFactory.create(full_name='National Hockey League')
+    def test_full_name_different_sports(self):
+        # No error should be thrown, sports are different
+        LeagueFactory.create(full_name='My League', sport=self.ice_hockey)
+        LeagueFactory.create(full_name='My League', sport=SportFactory.create(name='My Other Sport'))
+
+    def test_full_name_unique_with_sport(self):
+        LeagueFactory.create(full_name='National Hockey League', sport=self.ice_hockey)
         with self.assertRaises(IntegrityError, msg='UNIQUE constraint failed: leagues_league.full_name'):
-            LeagueFactory.create(full_name='National Hockey League')
+            LeagueFactory.create(full_name='National Hockey League', sport=self.ice_hockey).full_clean(exclude=['abbreviated_name'])
 
     def test_abbreviated_name_lowercase(self):
         # Make sure first letters are capitalized
-        nhl = LeagueFactory.create(full_name='national Hockey league')
+        nhl = LeagueFactory.create(full_name='national Hockey league', sport=self.ice_hockey)
+        nhl.full_clean(exclude=['abbreviated_name'])
         self.assertEqual(nhl.abbreviated_name, 'NHL')
 
     def test_abbreviated_name_normal(self):
-        liahl = LeagueFactory.create(full_name='Long Island Amateur Hockey League')
+        liahl = LeagueFactory.create(full_name='Long Island Amateur Hockey League', sport=self.ice_hockey)
+        liahl.full_clean(exclude=['abbreviated_name'])
         self.assertEqual(liahl.abbreviated_name, 'LIAHL')
 
     def test_abbreviated_name_one_word(self):
-        fake_league = LeagueFactory.create(full_name='National')
+        fake_league = LeagueFactory.create(full_name='National', sport=self.ice_hockey)
+        fake_league.full_clean(exclude=['abbreviated_name'])
         self.assertEqual(fake_league.abbreviated_name, 'N')
 
     def test_abbreviated_name_extra_whitespace(self):
-        fake_league = LeagueFactory.create(full_name=' League With  Extra  Space')
+        fake_league = LeagueFactory.create(full_name=' League With  Extra  Space', sport=self.ice_hockey)
+        fake_league.full_clean(exclude=['abbreviated_name'])
         self.assertEqual(fake_league.abbreviated_name, 'LWES')
