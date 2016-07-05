054e62f180377bb25f14edb31af5c173ac40dd66
Harris Pittinsky <hpittin1@binghamton.edu>
2016-07-05 19:40:25 -0400

    add slug to divisions, update dev_only fixture to include division slugs

--- apps/divisions/admin.py
+++ apps/divisions/admin.py
@@ -3,9 +3,10 @@ from .models import Division
 
 
 class DivisionAdmin(admin.ModelAdmin):
-    list_display = ['id', 'name', 'league']
+    list_display = ['id', 'name', 'slug', 'league']
     list_display_links = ['name']
-    search_fields = ['name']
+    search_fields = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
 
 
 admin.site.register(Division, DivisionAdmin)
--- apps/divisions/models.py
+++ apps/divisions/models.py
@@ -1,15 +1,22 @@
 from django.db import models
+from django.utils.text import slugify
+
 from leagues.models import League
 
 
 class Division(models.Model):
     name = models.CharField(max_length=128, verbose_name='Name')
+    slug = models.SlugField(null=True, blank=True, verbose_name='Slug')
     league = models.ForeignKey(League)
+
+    def clean(self):
+        self.slug = slugify(self.name)
 
     class Meta:
         ordering = ['name']
         unique_together = (
             ('name', 'league'),
+            ('slug', 'league')
         )
 
     def __str__(self):
