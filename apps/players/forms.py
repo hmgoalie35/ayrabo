ae354fcd37bdf9c7805697a692bed32ca57b1ba3
Harris Pittinsky <hpittin1@binghamton.edu>
2016-07-31 17:17:51 -0400

    add in basketball player form

--- apps/players/forms.py
+++ apps/players/forms.py
@@ -1,25 +1,36 @@
 from django import forms
 
-from .models import HockeyPlayer, BaseballPlayer
+from . import models
 
 
 class HockeyPlayerForm(forms.ModelForm):
     prefix = 'hockeyplayer'
 
-    jersey_number = forms.IntegerField(min_value=HockeyPlayer.MIN_JERSEY_NUMBER,
-                                       max_value=HockeyPlayer.MAX_JERSEY_NUMBER)
+    jersey_number = forms.IntegerField(min_value=models.HockeyPlayer.MIN_JERSEY_NUMBER,
+                                       max_value=models.HockeyPlayer.MAX_JERSEY_NUMBER)
 
     class Meta:
-        model = HockeyPlayer
+        model = models.HockeyPlayer
         exclude = ['user', 'sport']
 
 
 class BaseballPlayerForm(forms.ModelForm):
     prefix = 'baseballplayer'
 
-    jersey_number = forms.IntegerField(min_value=BaseballPlayer.MIN_JERSEY_NUMBER,
-                                       max_value=BaseballPlayer.MAX_JERSEY_NUMBER)
+    jersey_number = forms.IntegerField(min_value=models.BaseballPlayer.MIN_JERSEY_NUMBER,
+                                       max_value=models.BaseballPlayer.MAX_JERSEY_NUMBER)
 
     class Meta:
-        model = BaseballPlayer
+        model = models.BaseballPlayer
+        exclude = ['user', 'sport']
+
+
+class BasketballPlayerForm(forms.ModelForm):
+    prefix = 'basketballplayer'
+
+    jersey_number = forms.IntegerField(min_value=models.BasketballPlayer.MIN_JERSEY_NUMBER,
+                                       max_value=models.BasketballPlayer.MAX_JERSEY_NUMBER)
+
+    class Meta:
+        model = models.BasketballPlayer
         exclude = ['user', 'sport']
