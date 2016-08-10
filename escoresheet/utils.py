0401cc8312910cc321b175e3338b122302a35bd5
Harris Pittinsky <hpittin1@binghamton.edu>
2016-08-09 23:57:38 -0400

    add in docs for utils file

--- escoresheet/utils.py
+++ escoresheet/utils.py
@@ -7,7 +7,6 @@ def remove_form_placeholders(field_list):
     """
     Django all-auth by default adds in annoying placeholders, so remove them from all fields in the given list
     :param field_list: A list of fields to have placeholders removed
-    :return:
     """
     for field_name in field_list:
         attributes = field_list.get(field_name).widget.attrs
@@ -21,9 +20,10 @@ def add_autofocus_to_field(field):
 
 def set_fields_disabled(read_only_fields, field_list):
     """
-
-    :param field_list:
-    :return:
+    Given a list of form fields, sets them to disabled so the HTML form being rendered shows them as disabled.
+    Django will also ignore any POST data that has the field's name attribute in it (in the case of tampering)
+    :param read_only_fields: The fields to mark as disabled
+    :param field_list: The list of fields of the form
     """
     for field in read_only_fields:
         the_field = field_list.get(field, None)
