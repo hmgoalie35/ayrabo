a74bb84b4fce3be987324aa7aaf9419d74611632
Harris Pittinsky <hpittin1@binghamton.edu>
2016-10-03 16:52:41 -0400

    Fixes 71

--- apps/seasons/forms.py
+++ apps/seasons/forms.py
@@ -7,18 +7,18 @@ from .models import Season
 class SeasonAdminForm(forms.ModelForm):
     class Meta:
         model = Season
-        fields = ['division', 'start_date', 'end_date', 'teams']
+        fields = ['league', 'start_date', 'end_date', 'teams']
 
     def clean(self):
-        division = self.cleaned_data.get('division', None)
+        league = self.cleaned_data.get('league', None)
         teams = self.cleaned_data.get('teams', None)
         errors = {'teams': []}
-        if division and teams:
+        if league and teams:
             for team in teams:
-                if team.division_id != division.id:
+                if team.league_id != league.id:
                     errors['teams'].append(
-                        'The team specified ({team_name}) does not belong to {division} - {league}'.format(
-                            team_name=team.name, division=division.name, league=division.league.full_name))
+                        'The team specified ({team_name}) does not belong to {league}'.format(
+                            team_name=team.name, league=league.full_name))
 
             if errors['teams']:
                 raise ValidationError(errors)
