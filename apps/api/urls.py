8204d299ce1bd96ac47b0fdce2781bd1f32f0f76
Harris Pittinsky <hpittin1@binghamton.edu>
2016-11-09 21:57:39 -0500

    disable api documentation, change sports to sport in drf endpoint

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -3,10 +3,10 @@ from rest_framework.authtoken.views import obtain_auth_token
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.APIHomeView.as_view(), name='api_home'),
+    # url(r'^$', views.APIHomeView.as_view(), name='api_home'),
 
     url('^auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url('^token/$', obtain_auth_token, name='obtain_api_token'),
+    url('^token/obtain/$', obtain_auth_token, name='obtain_api_token'),
     url('^token/revoke/$', views.RevokeAuthTokenView.as_view(), name='revoke_api_token'),
 
     url(r'^v1/', include('api.v1.urls', namespace='v1')),
--- apps/api/views.py
+++ apps/api/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import get_object_or_404
 from rest_framework import generics
 from rest_framework.authtoken.models import Token
+from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import AllowAny
 from rest_framework.renderers import TemplateHTMLRenderer
 from rest_framework.response import Response
