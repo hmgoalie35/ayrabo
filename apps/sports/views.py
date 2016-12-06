73a682776e8286c58e316700bd2155801318e5bb
Harris Pittinsky <hpittin1@binghamton.edu>
2016-12-05 22:27:30 -0500

    replace staticfiles tag w/ static tag, fix inlineformset api change

--- apps/sports/views.py
+++ apps/sports/views.py
@@ -163,7 +163,10 @@ class FinishSportRegistrationView(LoginRequiredMixin, ContextMixin, generic.View
 
 class SportRegistrationInlineFormSet(BaseInlineFormSet):
     def clean(self):
-        super(SportRegistrationInlineFormSet, self).clean()
+        # Django 1.10.x adds in unique_constraint validation specified by the model the form is for.
+        # I am currently disabling this because I had already implemented something similar and using the native
+        # method was showing unhelpful error messages that would confused the user.
+        # super(SportRegistrationInlineFormSet, self).clean()
         sports_already_seen = []
         for form in self.forms:
             sport = form.cleaned_data.get('sport')
