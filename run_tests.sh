8f114268de11881aeb416afa5743167c211ca8ac
Harris Pittinsky <hpittin1@binghamton.edu>
2016-12-13 22:36:55 -0500

    ignore venv for coverage and flake, update testing script

--- run_tests.sh
+++ run_tests.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
+set -e
+
 print_step () {
     printf "\n\n>>> $1\n\n"
 }
+
+COVERAGE_MIN=94
 
 # Need to activate the virtualenv when running tests locally
 if [ -e venv ]; then
@@ -10,12 +14,11 @@ if [ -e venv ]; then
 fi
 
 print_step "Running unit/functional tests"
+coverage erase
 coverage run manage.py test --parallel --failfast
-coverage combine && coverage report
+coverage combine && coverage report --fail-under=${COVERAGE_MIN}
 
-# TODO Run coverage for behave also?
-#coverage erase
 
+# Doesn't really make sense to run coverage for behave. Those tests are just meant to check basic usability
 print_step "Running integration tests"
 python manage.py behave
-
