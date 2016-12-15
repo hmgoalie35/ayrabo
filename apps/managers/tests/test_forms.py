8c92dcff0c3f2acb70dd1586e42a6a38b115ce5a
Harris Pittinsky <hpittin1@binghamton.edu>
2016-12-14 21:37:50 -0500

    add coach and manager form tests

---
+++ apps/managers/tests/test_forms.py
@@ -0,0 +1,41 @@
+from divisions.tests import DivisionFactory
+from escoresheet.utils import BaseTestCase
+from leagues.tests import LeagueFactory
+from managers.forms import ManagerForm
+from sports.tests import SportFactory
+from teams.tests import TeamFactory
+
+
+class ManagerFormTests(BaseTestCase):
+    def setUp(self):
+        self.sport = SportFactory(name='Ice Hockey')
+        self.league = LeagueFactory(sport=self.sport, full_name='Long Island Amateur Hockey League')
+        self.division = DivisionFactory(league=self.league, name='Midget Minor AA')
+        self.form_cls = ManagerForm
+
+    def test_teams_filtered_by_sport(self):
+        TeamFactory.create_batch(5, division=self.division)
+        team_different_sport = TeamFactory()
+        form = self.form_cls(sport=self.sport)
+
+        team_field = form.fields['team']
+
+        self.assertNotIn(team_different_sport, team_field.queryset)
+
+    def test_no_sport_kwarg(self):
+        teams = TeamFactory.create_batch(2, division=self.division)
+        team_different_sport = TeamFactory()
+        form = self.form_cls()
+        team_field = form.fields['team']
+
+        teams.append(team_different_sport)
+
+        self.assertTrue(set(teams).issubset(set(team_field.queryset)))
+
+    def test_fields_set_to_disabled(self):
+        form = self.form_cls(read_only_fields=['team'])
+        self.assertTrue(form.fields['team'].disabled)
+
+    def test_no_read_only_fields_kwarg(self):
+        form = self.form_cls()
+        self.assertFalse(form.fields['team'].disabled)
