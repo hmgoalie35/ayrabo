ca5af93fd9bb69209107703106a001d1340aa3e4
Harris Pittinsky <hpittin1@binghamton.edu>
2016-12-14 21:38:20 -0500

    DRY out player forms, add in form tests

--- apps/players/forms.py
+++ apps/players/forms.py
@@ -5,23 +5,30 @@ from teams.models import Team
 from . import models
 
 
-class HockeyPlayerForm(forms.ModelForm):
-    prefix = 'hockeyplayer'
+class PlayerForm(forms.ModelForm):
+    """
+    Base form class for all player forms.
+    """
 
     def __init__(self, *args, **kwargs):
         """
-        Override the constructor to allow for custom kwargs to be passed to this form.
-        sport -- The sport to filter teams by
-        read_only_fields -- A list of fields that should be disabled in an HTML form. Note that using this will make
-        django think the form 'has_changed' when it really hasn't.
+        Accepts the following kwargs
+
+        :sport: The sport to filter teams by
+        :read_only_fields: List of fields that should be disabled in an HTML form. Note that using this will make
+            django think the form 'has_changed' when it really hasn't.
         """
         sport = kwargs.pop('sport', None)
         read_only_fields = kwargs.pop('read_only_fields', None)
-        super(HockeyPlayerForm, self).__init__(*args, **kwargs)
+        super(PlayerForm, self).__init__(*args, **kwargs)
         if sport is not None:
             self.fields['team'].queryset = Team.objects.filter(division__league__sport=sport).select_related('division')
         if read_only_fields is not None:
             set_fields_disabled(read_only_fields, self.fields)
+
+
+class HockeyPlayerForm(PlayerForm):
+    prefix = 'hockeyplayer'
 
     jersey_number = forms.IntegerField(min_value=models.HockeyPlayer.MIN_JERSEY_NUMBER,
                                        max_value=models.HockeyPlayer.MAX_JERSEY_NUMBER)
@@ -30,26 +37,11 @@ class HockeyPlayerForm(forms.ModelForm):
 
     class Meta:
         model = models.HockeyPlayer
-        exclude = ['user', 'sport']
+        fields = ['team', 'jersey_number', 'position', 'handedness']
 
 
-class BaseballPlayerForm(forms.ModelForm):
+class BaseballPlayerForm(PlayerForm):
     prefix = 'baseballplayer'
-
-    def __init__(self, *args, **kwargs):
-        """
-        Override the constructor to allow for custom kwargs to be passed to this form.
-        sport -- The sport to filter teams by
-        read_only_fields -- A list of fields that should be disabled in an HTML form. Note that using this will make
-        django think the form 'has_changed' when it really hasn't.
-        """
-        sport = kwargs.pop('sport', None)
-        read_only_fields = kwargs.pop('read_only_fields', None)
-        super(BaseballPlayerForm, self).__init__(*args, **kwargs)
-        if sport is not None:
-            self.fields['team'].queryset = Team.objects.filter(division__league__sport=sport).select_related('division')
-        if read_only_fields is not None:
-            set_fields_disabled(read_only_fields, self.fields)
 
     jersey_number = forms.IntegerField(min_value=models.BaseballPlayer.MIN_JERSEY_NUMBER,
                                        max_value=models.BaseballPlayer.MAX_JERSEY_NUMBER)
@@ -58,31 +50,17 @@ class BaseballPlayerForm(forms.ModelForm):
 
     class Meta:
         model = models.BaseballPlayer
-        exclude = ['user', 'sport']
+        fields = ['team', 'jersey_number', 'position', 'catches', 'bats']
 
 
-class BasketballPlayerForm(forms.ModelForm):
+class BasketballPlayerForm(PlayerForm):
     prefix = 'basketballplayer'
-
-    def __init__(self, *args, **kwargs):
-        """
-        Override the constructor to allow for custom kwargs to be passed to this form.
-        sport -- The sport to filter teams by
-        read_only_fields -- A list of fields that should be disabled in an HTML form. Note that using this will make
-        django think the form 'has_changed' when it really hasn't.
-        """
-        sport = kwargs.pop('sport', None)
-        read_only_fields = kwargs.pop('read_only_fields', None)
-        super(BasketballPlayerForm, self).__init__(*args, **kwargs)
-        if sport is not None:
-            self.fields['team'].queryset = Team.objects.filter(division__league__sport=sport).select_related('division')
-        if read_only_fields is not None:
-            set_fields_disabled(read_only_fields, self.fields)
 
     jersey_number = forms.IntegerField(min_value=models.BasketballPlayer.MIN_JERSEY_NUMBER,
                                        max_value=models.BasketballPlayer.MAX_JERSEY_NUMBER)
+
     team = TeamModelChoiceField(queryset=Team.objects.all().select_related('division'))
 
     class Meta:
         model = models.BasketballPlayer
-        exclude = ['user', 'sport']
+        fields = ['team', 'jersey_number', 'position', 'shoots']
