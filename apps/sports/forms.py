69bce53f9e3d3df13a67185a653e879d063c163d
Harris Pittinsky <hpittin1@binghamton.edu>
2016-12-16 20:13:13 -0500

    add sport reg form tests

--- apps/sports/forms.py
+++ apps/sports/forms.py
@@ -1,15 +1,15 @@
 from django import forms
 from django.utils.translation import ugettext_lazy as _
 
-from . import models
+from .models import Sport, SportRegistration
 
 
 class SportRegistrationAdminForm(forms.ModelForm):
-    roles = forms.MultipleChoiceField(choices=[(role, role) for role in models.SportRegistration.ROLES],
+    roles = forms.MultipleChoiceField(choices=[(role, role) for role in SportRegistration.ROLES],
                                       widget=forms.CheckboxSelectMultiple)
 
     class Meta:
-        model = models.SportRegistration
+        model = SportRegistration
         fields = ['user', 'sport', 'is_complete']
         help_texts = {
             'roles_mask': _(
@@ -21,19 +21,20 @@ class CreateSportRegistrationForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         """
         Override the constructor to allow for additional kwargs to be passed in.
-        sport_already_registered_for -- a list of sport ids the current user has already registered for (so they can't)
-        register for that sport again.
+
+        :sport_already_registered_for: a list of sport ids the current user has already registered for (so they can't)
+          register for that sport again.
         """
         sports_already_registered_for = kwargs.pop('sports_already_registered_for', None)
         super(CreateSportRegistrationForm, self).__init__(*args, **kwargs)
         if sports_already_registered_for is not None:
-            self.fields['sport'].queryset = models.Sport.objects.all().exclude(id__in=sports_already_registered_for)
+            self.fields['sport'].queryset = Sport.objects.all().exclude(id__in=sports_already_registered_for)
 
-    roles = forms.MultipleChoiceField(choices=[(role, role) for role in models.SportRegistration.ROLES],
+    roles = forms.MultipleChoiceField(choices=[(role, role) for role in SportRegistration.ROLES],
                                       widget=forms.CheckboxSelectMultiple)
 
     field_order = ['sport', 'roles']
 
     class Meta:
-        model = models.SportRegistration
+        model = SportRegistration
         fields = ['sport']
---
+++ apps/sports/tests/test_forms.py
@@ -0,0 +1,25 @@
+from escoresheet.utils import BaseTestCase
+from sports.forms import CreateSportRegistrationForm
+from sports.tests import SportFactory, SportRegistrationFactory
+from accounts.tests import UserFactory
+
+
+class CreateSportRegistrationFormTests(BaseTestCase):
+    def setUp(self):
+        self.user = UserFactory()
+        self.hockey = SportFactory(name='Ice Hockey')
+        self.soccer = SportFactory(name='Soccer')
+        self.lax = SportFactory(name='Lacrosse')
+        self.hockey_reg = SportRegistrationFactory(user=self.user, sport=self.hockey)
+        self.soccer_reg = SportRegistrationFactory(user=self.user, sport=self.soccer)
+
+        self.form_cls = CreateSportRegistrationForm
+
+    def test_sets_fields_disabled(self):
+        form = self.form_cls(sports_already_registered_for=[self.hockey_reg.id, self.soccer_reg.id])
+
+        sport_field = form.fields['sport']
+        qs = sport_field.queryset
+        self.assertIn(self.lax, qs)
+        self.assertNotIn(self.hockey, qs)
+        self.assertNotIn(self.soccer_reg, qs)
