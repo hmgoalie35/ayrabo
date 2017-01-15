041a743794af718c52a4de1af6fc919ada4a9221
Harris Pittinsky <hpittin1@binghamton.edu>
2017-01-15 15:53:26 -0500

    fix function that checks if file was changed in last commit

--- sync_project.sh
+++ sync_project.sh
@@ -5,25 +5,34 @@ print_step () {
 }
 
 was_changed_in_last_commit () {
-    git diff-tree --name-only -r HEAD | grep "$1"
+    git diff-tree --name-only -r HEAD HEAD~ | grep -e "^$1$"
 }
 
-if [[ $(was_changed_in_last_commit "package.json") == 0 ]]; then
+if [[ $(was_changed_in_last_commit "package.json") ]]; then
     print_step "Rebuilding npm packages"
     rm -rf node_modules
     npm install
+else
+    print_step "No new npm packages"
 fi
 
-if [[ $(was_changed_in_last_commit "requirements.txt") == 0 ]]; then
+if [[ $(was_changed_in_last_commit "requirements.txt") ]]; then
     print_step "Rebuilding virtualenv"
     rm -rf venv
     virtualenv venv -p $(which python3)
     source venv/bin/activate
     pip install -r requirements.txt
+else
+    print_step "No new pypi packages"
 fi
 
 print_step "Running migrations"
 python manage.py migrate
 
-print_step "Applying fixtures"
-python manage.py loaddata dev_fixtures
+
+if [[ $(was_changed_in_last_commit "dev_fixtures.json") ]]; then
+    print_step "Applying fixtures"
+    python manage.py loaddata dev_fixtures
+fi
+
+print_step "Done"
