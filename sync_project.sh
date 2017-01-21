52420525cd26a9a7844147ba9c19a056d80e50c9
Harris Pittinsky <hpittin1@binghamton.edu>
2017-01-20 20:39:31 -0500

    update requirements, always rebuild node_modules and venv when syncing project

--- sync_project.sh
+++ sync_project.sh
@@ -8,31 +8,27 @@ was_changed_in_last_commit () {
     git diff-tree --name-only -r HEAD HEAD~ | grep -e "^$1$"
 }
 
-if [[ $(was_changed_in_last_commit "package.json") ]]; then
-    print_step "Rebuilding npm packages"
-    rm -rf node_modules
-    npm install
-else
-    print_step "No new npm packages"
-fi
+# For reference
 
-if [[ $(was_changed_in_last_commit "requirements.txt") ]]; then
-    print_step "Rebuilding virtualenv"
-    rm -rf venv
-    virtualenv venv -p $(which python3)
-    source venv/bin/activate
-    pip install -r requirements.txt
-else
-    print_step "No new pypi packages"
-fi
+#if [[ $(was_changed_in_last_commit "dev_fixtures.json") ]]; then
+#    print_step "Applying fixtures"
+#    python manage.py loaddata dev_fixtures
+#fi
+
+print_step "Rebuilding npm packages"
+rm -rf node_modules
+npm install
+
+print_step "Rebuilding virtualenv"
+rm -rf venv
+virtualenv venv -p $(which python3)
+source venv/bin/activate
+pip install -r requirements.txt
 
 print_step "Running migrations"
 python manage.py migrate
 
-
-if [[ $(was_changed_in_last_commit "dev_fixtures.json") ]]; then
-    print_step "Applying fixtures"
-    python manage.py loaddata dev_fixtures
-fi
+print_step "Applying fixtures"
+python manage.py loaddata dev_fixtures
 
 print_step "Done"
