8c4a8757a978b5c6ec0d75bc0e3f59830577e8ac
Harris Pittinsky <harris@pittinsky.com>
2017-03-11 21:08:30 -0500

    Add select related call

--- apps/referees/forms.py
+++ apps/referees/forms.py
@@ -21,7 +21,7 @@ class RefereeForm(forms.ModelForm):
         read_only_fields = kwargs.pop('read_only_fields', None)
         super(RefereeForm, self).__init__(*args, **kwargs)
         if sport is not None:
-            self.fields['league'].queryset = League.objects.filter(sport=sport)
+            self.fields['league'].queryset = League.objects.filter(sport=sport).select_related('sport')
         if read_only_fields is not None:
             set_fields_disabled(read_only_fields, self.fields)
 
