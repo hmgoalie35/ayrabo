7a1e040280f73406448d1b8fa0eeeccfe4535188
Harris Pittinsky <harris@pittinsky.com>
2017-04-08 21:16:08 -0400

    Implement updating of players

--- apps/players/admin.py
+++ apps/players/admin.py
@@ -7,15 +7,18 @@ from . import models
 class HockeyPlayerAdmin(admin.ModelAdmin):
     list_display = ['id', 'user', 'position', 'handedness', 'jersey_number', 'team', 'division', 'league', 'sport']
     list_display_links = ['user']
+    search_fields = ['id', 'user__email', 'user__first_name', 'user__last_name', 'jersey_number', 'team__name']
 
 
 @admin.register(models.BaseballPlayer)
 class BaseballPlayerAdmin(admin.ModelAdmin):
     list_display = ['id', 'user', 'position', 'catches', 'bats', 'jersey_number', 'team', 'division', 'league', 'sport']
     list_display_links = ['user']
+    search_fields = ['id', 'user__email', 'user__first_name', 'user__last_name', 'jersey_number', 'team__name']
 
 
 @admin.register(models.BasketballPlayer)
 class BasketballPlayerAdmin(admin.ModelAdmin):
     list_display = ['id', 'user', 'team', 'position', 'shoots', 'jersey_number', 'team', 'division', 'league', 'sport']
     list_display_links = ['user']
+    search_fields = ['id', 'user__email', 'user__first_name', 'user__last_name', 'jersey_number', 'team__name']
--- apps/players/urls.py
+++ apps/players/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^create/$', views.CreatePlayersView.as_view(), name='create'),
+    url(r'^(?P<player_pk>\d+)/update/$', views.UpdatePlayerView.as_view(), name='update'),
 ]
