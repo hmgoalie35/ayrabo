04c34745ced3c82cbfbc143db11ca200423a8b51
Harris Pittinsky <harris@pittinsky.com>
2017-05-14 20:56:36 -0400

    Add allowed hosts to env file, add ansible db role, update settings to use env file settings

---
+++ devops/ansible/devops.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import argparse
+
+
+def get_parser():
+    parser = argparse.ArgumentParser()
+    return parser
+
+
+def main():
+    parser = get_parser()
+    print(parser)
+
+
+if __name__ == '__main__':
+    main()
