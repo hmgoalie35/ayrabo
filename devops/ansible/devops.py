9ef027b272160f61c45f6b5b5f2cc2f1553051d1
Harris Pittinsky <harris@pittinsky.com>
2017-05-21 23:18:51 -0400

    WIP adding web role, need deploy role first

--- devops/ansible/devops.py
+++ devops/ansible/devops.py
@@ -9,6 +9,16 @@ def get_parser():
 
 
 def main():
+    """
+    A wrapper around using ansible-playbook [options] on the command line all of the time.
+
+    TODO:
+        1. Maintenance mode
+        2. Deploying arbitrary branches (collect static, migrate, etc.) to arbitrary servers
+        3. Rollback
+        4. DB backups/restores
+        5. Applying new nginx conf (Just need to run web role...)
+    """
     parser = get_parser()
     print(parser)
 
