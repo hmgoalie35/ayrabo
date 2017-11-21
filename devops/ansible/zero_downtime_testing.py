3fd0ce2e16615a878ddccb4a223566ea763f9269
Harris Pittinsky <harris@pittinsky.com>
2017-11-20 22:26:12 -0500

    Update packages, add deployment testing script

---
+++ devops/ansible/zero_downtime_testing.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+import time
+
+import requests
+
+SITE_URL = 'https://192.168.1.88/'
+
+
+def print_msg(msg):
+    print()
+    print('#' * 10)
+    print(msg)
+    print('#' * 10)
+    print()
+
+
+def main():
+    while True:
+        resp = requests.get(SITE_URL, verify=False)
+        if resp.ok:
+            print_msg('SUCCESS')
+            time.sleep(3)
+        else:
+            print_msg('ERROR: {}'.format(resp.status_code))
+            break
+    print('Exiting...')
+
+
+if __name__ == '__main__':
+    main()
