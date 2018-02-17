65635cd3ef1901415e0ebcfb4999495d7dbb7f05
Harris Pittinsky <harris@pittinsky.com>
2018-02-16 19:23:38 -0500

    Update game roster component functionality, add tests

--- static/jsx/gameRosters/GameRosterComponent.jsx
+++ static/jsx/gameRosters/GameRosterComponent.jsx
@@ -1,42 +1,77 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Typeahead } from 'react-bootstrap-typeahead';
+
 
 import PlayerComponent from './PlayerComponent';
 import { playerPropType } from '../common/proptypes';
 import Loading from '../common/Loading';
+import { pluralize } from '../common/utils';
 
 
 export default class GameRosterComponent extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
+    this.getOptions = this.getOptions.bind(this);
+    this.handleTypeaheadChange = this.handleTypeaheadChange.bind(this);
+  }
+
+  getOptions(selectedPlayers, allPlayers) {
+    const ids = selectedPlayers.map(player => player.id);
+    return allPlayers.filter(player => !ids.includes(player.id));
+  }
+
+  // The param for this function is an array
+  handleTypeaheadChange(selected) {
+    const { handleAddPlayer } = this.props;
+    // See https://github.com/ericgio/react-bootstrap-typeahead/blob/master/docs/Methods.md#clear
+    if (selected.length) {
+      this.typeahead.getInstance().clear();
+    }
+    handleAddPlayer(selected);
   }
 
   render() {
     const {
       team,
-      players,
+      selectedPlayers,
+      allPlayers,
       canUpdate,
       teamType,
+      handleRemovePlayer,
     } = this.props;
 
     let element;
-    if (players === null) {
+    if (selectedPlayers === null) {
       element = (<div className="mt20"><Loading /></div>);
-    } else if (players.length === 0) {
+    } else if (selectedPlayers.length === 0) {
       element = (
         <div className="list-group-item">
           There are no players on this roster.
         </div>
       );
+    } else if (!canUpdate) {
+      element = (
+        <div className="list-group-item">
+          This game roster is currently unavailable.
+        </div>
+      );
     } else {
       element = (
-        players.map(player =>
-          <PlayerComponent
-            key={player.id}
-            player={player}
-            canUpdate={canUpdate}
-          />)
+        <React.Fragment>
+          {selectedPlayers.map(player =>
+            <PlayerComponent
+              key={player.id}
+              player={player}
+              canUpdate={canUpdate}
+              handleRemovePlayer={handleRemovePlayer}
+            />)
+          }
+          <div className="text-center mt5">
+            {`${selectedPlayers.length} ${pluralize('player', selectedPlayers.length)}`}
+          </div>
+        </React.Fragment>
       );
     }
 
@@ -45,6 +80,24 @@ export default class GameRosterComponent extends React.Component {
         <div className="text-center">
           <h4>{teamType} Team</h4>
           <div className="mb10"><strong>{team}</strong></div>
+          {(selectedPlayers !== null && allPlayers !== null) &&
+          <div className="text-left mb10">
+            <Typeahead
+              multiple
+              highlightOnlyResult
+              selectHintOnEnter
+              onChange={this.handleTypeaheadChange}
+              options={this.getOptions(selectedPlayers, allPlayers)}
+              disabled={!canUpdate}
+              emptyLabel="No players found."
+              placeholder="Search players by first name, last name or jersey number"
+              paginationText="Display more players"
+              ref={(typeahead) => {
+                this.typeahead = typeahead;
+              }}
+            />
+          </div>
+          }
           <div className="list-group">
             {element}
           </div>
@@ -56,11 +109,15 @@ export default class GameRosterComponent extends React.Component {
 
 GameRosterComponent.propTypes = {
   team: PropTypes.string.isRequired,
-  players: PropTypes.arrayOf(playerPropType),
+  selectedPlayers: PropTypes.arrayOf(playerPropType),
+  allPlayers: PropTypes.arrayOf(playerPropType),
   canUpdate: PropTypes.bool.isRequired,
   teamType: PropTypes.oneOf(['Home', 'Away']).isRequired,
+  handleAddPlayer: PropTypes.func.isRequired,
+  handleRemovePlayer: PropTypes.func.isRequired,
 };
 
 GameRosterComponent.defaultProps = {
-  players: null,
+  selectedPlayers: null,
+  allPlayers: null,
 };
--- static/jsx/gameRosters/__tests__/GameRosterComponent.test.jsx
+++ static/jsx/gameRosters/__tests__/GameRosterComponent.test.jsx
@@ -5,39 +5,77 @@ import GameRosterComponent from '../GameRosterComponent';
 import homePlayers from './homePlayers.json';
 
 
-const getComponent = (players = null) => {
+const addTypeaheadLabel = players =>
+  players.map(p => ({
+    ...p,
+    label: `#${p.jersey_number} ${p.user.first_name} ${p.user.last_name}`,
+  }));
+
+const playersWithLabel = addTypeaheadLabel(homePlayers);
+
+const getComponent = (selectedPlayers, allPlayers, canUpdate = true, handleAddPlayer = jest.fn()) => {
   const props = {
     team: 'Green Machine IceCats Midget Minor AA',
-    canUpdate: true,
+    canUpdate,
     teamType: 'Home',
-    players,
+    selectedPlayers,
+    allPlayers,
+    handleAddPlayer,
+    handleRemovePlayer: jest.fn(),
   };
   return mount(<GameRosterComponent {...props} />);
 };
 
+describe('Component functions', () => {
+  test('getOptions', () => {
+    const allPlayers = playersWithLabel.slice(0, 5);
+    const selectedPlayers = playersWithLabel.slice(0, 2);
+    const component = getComponent(selectedPlayers, allPlayers);
+    const result = component.instance().getOptions(selectedPlayers, allPlayers);
+    expect(result).toEqual(allPlayers.slice(2, 5));
+  });
+
+  test('handleTypeaheadChange', () => {
+    const allPlayers = playersWithLabel.slice(0, 5);
+    const selectedPlayers = playersWithLabel.slice(0, 2);
+    const player = playersWithLabel.slice(2, 3);
+    const handleAddPlayer = jest.fn();
+    const component = getComponent(selectedPlayers, allPlayers, true, handleAddPlayer);
+    component.instance().handleTypeaheadChange([player]);
+    expect(handleAddPlayer).toHaveBeenCalledWith([player]);
+    // Could add a test to make sure .clear is called.
+  });
+});
+
 describe('render', () => {
   test('team type displayed', () => {
-    const component = getComponent();
+    const component = getComponent([], playersWithLabel);
     expect(component.find('h4').text()).toEqual('Home Team');
   });
 
   test('team name displayed', () => {
-    const component = getComponent();
+    const component = getComponent([], playersWithLabel);
     expect(component.find('strong').text()).toEqual('Green Machine IceCats Midget Minor AA');
   });
 
-  test('players null', () => {
-    const component = getComponent(null);
+  test('selectedPlayers null', () => {
+    const component = getComponent(null, playersWithLabel);
     expect(component.find('.list-group').find('i').hasClass('fa-spinner')).toBeTruthy();
   });
 
-  test('players empty', () => {
-    const component = getComponent([]);
+  test('selectedPlayers empty', () => {
+    const component = getComponent([], playersWithLabel);
     expect(component.find('.list-group').find('.list-group-item').text()).toEqual('There are no players on this roster.');
   });
 
-  test('players exist', () => {
-    const component = getComponent(homePlayers.slice(0, 5));
+  test('selectedPlayers exist, user can\'t update roster', () => {
+    const component = getComponent(playersWithLabel.slice(0, 5), playersWithLabel, false);
+    expect(component.find('.list-group-item').text()).toEqual('This game roster is currently unavailable.');
+  });
+
+  test('selectedPlayers exist, user can update roster', () => {
+    const component = getComponent(playersWithLabel.slice(0, 5), playersWithLabel);
     expect(component.find('.list-group-item').length).toEqual(5);
+    expect(component.find('.list-group .text-center').text()).toEqual('5 players');
   });
 });
