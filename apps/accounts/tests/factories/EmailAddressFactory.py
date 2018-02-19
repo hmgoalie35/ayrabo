eb9380e14a458d2af1f0f8ad811794dfc71ab475
Harris Pittinsky <harris@pittinsky.com>
2018-02-19 12:18:44 -0500

    Fix import

--- apps/accounts/tests/factories/EmailAddressFactory.py
+++ apps/accounts/tests/factories/EmailAddressFactory.py
@@ -2,14 +2,14 @@ import factory
 from allauth.account.models import EmailAddress
 from factory import django
 
-from users.tests.factories.UserFactory import UserFactory
+from users.tests import UserFactory
 
 
 class EmailAddressFactory(django.DjangoModelFactory):
-    class Meta:
-        model = EmailAddress
-
     user = factory.SubFactory(UserFactory)
     email = factory.lazy_attribute(lambda obj: obj.user.email)
     verified = True
     primary = True
+
+    class Meta:
+        model = EmailAddress
