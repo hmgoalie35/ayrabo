4ec918229aa77bef7854bb78698d95680b13d8fb
Harris Pittinsky <harris@pittinsky.com>
2018-05-13 15:13:49 -0400

    Add org FK to team, write migration to initially set org for all teams, use select related for team admin queries

--- apps/organizations/admin.py
+++ apps/organizations/admin.py
@@ -6,6 +6,5 @@ from .models import Organization
 @admin.register(Organization)
 class OrganizationAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'slug', 'created', 'updated')
-    list_filter = ('name',)
     search_fields = ('name', 'slug')
     prepopulated_fields = {'slug': ('name',)}
