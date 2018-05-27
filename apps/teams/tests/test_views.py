0520e859d6c4ae48e21d27f61a4423099452c479
Harris Pittinsky <harris@pittinsky.com>
2018-05-26 21:31:18 -0400

    Fix tests

--- apps/teams/tests/test_views.py
+++ apps/teams/tests/test_views.py
@@ -5,6 +5,10 @@ from django.core.files.uploadedfile import SimpleUploadedFile
 from django.urls import reverse
 
 from ayrabo.utils.testing import BaseTestCase
+from divisions.tests import DivisionFactory
+from leagues.tests import LeagueFactory
+from organizations.tests import OrganizationFactory
+from sports.tests import SportFactory
 from teams.models import Team
 from users.tests import UserFactory
 
@@ -18,17 +22,23 @@ class BulkUploadTeamsViewTests(BaseTestCase):
         self.user = UserFactory(email=self.email, password=self.password, is_staff=True)
 
     def test_post_valid_csv(self):
+        ice_hockey = SportFactory(name='Ice Hockey')
+        liahl = LeagueFactory(full_name='Long Island Amateur Hockey League', sport=ice_hockey)
+        DivisionFactory(name='10U Milner', league=liahl)
+        OrganizationFactory(name='Green Machine IceCats')
         self.client.login(email=self.email, password=self.password)
         with open(os.path.join(self.test_file_path, 'bulk_upload_teams_example.csv')) as f:
             response = self.client.post(self.url, {'file': f}, follow=True)
-            self.assertHasMessage(response, 'Successfully created 2 team object(s)')
-            self.assertEqual(Team.objects.count(), 2)
+            self.assertHasMessage(response, 'Successfully created 1 team object(s)')
+            self.assertEqual(Team.objects.count(), 1)
 
     def test_post_invalid_csv(self):
         self.client.login(email=self.email, password=self.password)
-        content = b'name, street_number, street, city, state, zip_code, phone_number, website\n\na,b,c,d,e,f,g,h'
+        content = b'name, website, division, organization\n\na,b,c,d'
         f = SimpleUploadedFile('test.csv', content)
         response = self.client.post(self.url, {'file': f}, follow=True)
         self.assertEqual(Team.objects.count(), 0)
-        self.assertFormsetError(response, 'formset', 0, 'website', ['Enter a valid URL.'])
-        self.assertFormsetError(response, 'formset', 0, 'phone_number', ['Enter a valid phone number.'])
+        self.assertFormsetError(response, 'formset', 0, 'division',
+                                ['Select a valid choice. That choice is not one of the available choices.'])
+        self.assertFormsetError(response, 'formset', 0, 'organization',
+                                ['Select a valid choice. That choice is not one of the available choices.'])
