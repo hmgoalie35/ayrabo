db78da4027259e7740a11540f686cc5b36448d11
Harris Pittinsky <harris@pittinsky.com>
2018-06-01 18:38:46 -0400

    Update contact us page

--- apps/home/views.py
+++ apps/home/views.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.views.generic import TemplateView
 
 
@@ -13,3 +14,11 @@ class AboutUsView(TemplateView):
 
 class ContactUsView(TemplateView):
     template_name = 'home/contact_us.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['support_contact'] = {
+            'name': 'Harris Pittinsky',
+            'email': settings.SUPPORT_EMAIL,
+        }
+        return context
