0abbd95e9565068283246e7cc6cdb6fe8f099650
Harris Pittinsky <harris@pittinsky.com>
2018-07-05 22:15:26 -0400

    Move sport url include to main urls file

--- ayrabo/urls.py
+++ ayrabo/urls.py
@@ -19,7 +19,7 @@ urlpatterns = [
     url(r'^contact-us/$', ContactUsView.as_view(), name='contact_us'),
     url(r'^account/', include('accounts.urls')),  # Use our custom allauth views
     url(r'^account/', include('allauth.urls')),
-    url(r'^', include('sports.urls')),
+    url(r'^sports/', include('sports.urls', namespace='sports')),
     url(r'^teams/', include('teams.urls', namespace='teams')),
     url(r'^locations/', include('locations.urls', namespace='locations')),
     url(r'^organizations/', include('organizations.urls', namespace='organizations')),
