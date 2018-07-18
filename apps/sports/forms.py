3602a32bdcae80061a519f099f534f71fdad535d
Harris Pittinsky <harris@pittinsky.com>
2018-07-17 21:55:09 -0400

    Update sport reg create form, formset, view to handle new structure

--- apps/sports/forms.py
+++ apps/sports/forms.py
@@ -1,17 +1,16 @@
 from django import forms
 
-from ayrabo.utils.formsets import BaseModelFormSet
+from ayrabo.utils.formsets import BaseFormSet
 from .models import Sport, SportRegistration
 
 
-class SportRegistrationCreateForm(forms.ModelForm):
+class SportRegistrationCreateForm(forms.Form):
     """
     This form can only be used in the admin if the kwarg specifying which sports have already been registered for is not
     specified. We don't know which user was selected in the admin, so trying to figure out what sports they were
     registered for won't work (if we move that logic to the form at some point). There is a unique constraint on the
     model that should help prevent any issues.
     """
-    field_order = ('sport', 'role')
 
     def __init__(self, *args, **kwargs):
         sports_already_registered_for = kwargs.pop('sports_already_registered_for', None)
@@ -19,20 +18,23 @@ class SportRegistrationCreateForm(forms.ModelForm):
         if sports_already_registered_for:
             self.fields['sport'].queryset = Sport.objects.exclude(id__in=sports_already_registered_for)
 
+    sport = forms.ModelChoiceField(queryset=Sport.objects.all())
+    roles = forms.MultipleChoiceField(choices=SportRegistration.ROLE_CHOICES, widget=forms.CheckboxSelectMultiple)
+
     class Meta:
-        model = SportRegistration
-        fields = ('sport', 'role')
+        fields = ('sport', 'roles')
 
 
-class SportRegistrationModelFormSet(BaseModelFormSet):
+class SportRegistrationFormSet(BaseFormSet):
     def clean(self):
-        super(SportRegistrationModelFormSet, self).clean()
+        super().clean()
         sports_already_seen = []
         for form in self.forms:
             sport = form.cleaned_data.get('sport')
-            if sport is not None:
-                if sport in sports_already_seen:
-                    form.add_error('sport', 'Only one form can have {sport} selected. '
-                                            'Choose another sport, or remove this form.'.format(sport=sport.name))
-                else:
-                    sports_already_seen.append(sport)
+            if sport is None:
+                continue
+            if sport in sports_already_seen:
+                error = '{} has already been selected. Choose another sport or remove this form.'.format(sport.name)
+                form.add_error('sport', error)
+            else:
+                sports_already_seen.append(sport)
