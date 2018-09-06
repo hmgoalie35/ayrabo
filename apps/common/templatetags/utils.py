f6c0854d2919d278323713db626c1b524d0afe63
Harris Pittinsky <harris@pittinsky.com>
2018-09-05 20:56:29 -0400

    Rename template tag module, add some useful template tags, make auto dropup js available throughout the site

---
+++ apps/common/templatetags/utils.py
@@ -0,0 +1,38 @@
+import json
+
+from django import template
+
+register = template.Library()
+
+
+@register.filter
+def booltojson(value):  # pragma: no cover
+    """
+    Converts python booleans to javascript booleans.
+    See https://code.djangoproject.com/ticket/17419 for why we don't have a generic `tojson` filter.
+    """
+    if value in [True, False]:
+        return json.dumps(value)
+    return json.dumps(None)
+
+
+@register.simple_tag
+def pluralize_role(role):
+    mappings = {
+        'coach': 'Coaches',
+        'manager': 'Managers',
+        'player': 'Players',
+        'referee': 'Referees',
+        'scorekeeper': 'Scorekeepers'
+    }
+    return mappings.get(role.lower())
+
+
+@register.simple_tag(takes_context=True)
+def get_tab_active_class(context, comparator):
+    forloop = context.get('forloop')
+    is_first_iteration = forloop.get('first', False) if forloop is not None else False
+    active_tab = context.get('active_tab')
+    if is_first_iteration and active_tab is None or active_tab == comparator:
+        return 'active'
+    return ''
