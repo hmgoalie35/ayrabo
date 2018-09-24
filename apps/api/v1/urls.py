fffc62b23cca2e2d039239414955667e72ba60e4
Harris Pittinsky <harris@pittinsky.com>
2018-09-23 21:09:33 -0400

    Remove incomplete api docs, clean up token api tests

--- apps/api/v1/urls.py
+++ apps/api/v1/urls.py
@@ -1,9 +1,6 @@
 from django.conf.urls import include, url
 
-from . import views
-
 urlpatterns = [
-    url(r'^$', views.V1View.as_view(), name='v1_home'),
     url(r'^sports/', include('api.v1.sports.urls', namespace='sports')),
     url(r'^teams/', include('api.v1.teams.urls', namespace='teams')),
 ]
