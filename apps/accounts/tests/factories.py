46813b55defc4859b8a8155a6018c26463d68777
Harris Pittinsky <harris@pittinsky.com>
2018-10-30 22:41:26 -0400

    Simplify factories in accounts app

--- apps/accounts/tests/factories.py
+++ apps/accounts/tests/factories.py
@@ -1,13 +1,12 @@
-import factory
 from allauth.account.models import EmailAddress
-from factory import django
+from factory import LazyAttribute, SubFactory, django
 
 from users.tests import UserFactory
 
 
 class EmailAddressFactory(django.DjangoModelFactory):
-    user = factory.SubFactory(UserFactory)
-    email = factory.lazy_attribute(lambda obj: obj.user.email)
+    user = SubFactory(UserFactory)
+    email = LazyAttribute(lambda obj: obj.user.email)
     verified = True
     primary = True
 
