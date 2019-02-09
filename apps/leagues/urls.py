2b78e90c91b9a2b2a8e0626c9001ee51073198de
Harris Pittinsky <harris@pittinsky.com>
2019-02-09 14:21:54 -0500

    Consolidate league details views and season detail views

--- apps/leagues/urls.py
+++ apps/leagues/urls.py
@@ -1,11 +1,15 @@
 from django.conf.urls import include, url
 
-from seasons.urls import season_urls
 from . import views
 
+
+season_urls = [
+    url(r'^$', views.LeagueDetailScheduleView.as_view(), name='schedule'),
+    url(r'^divisions/$', views.LeagueDetailDivisionsView.as_view(), name='divisions'),
+]
 
 urlpatterns = [
     url(r'^(?P<slug>[-\w]+)/$', views.LeagueDetailScheduleView.as_view(), name='schedule'),
     url(r'^(?P<slug>[-\w]+)/divisions/$', views.LeagueDetailDivisionsView.as_view(), name='divisions'),
-    url(r'^(?P<slug>[-\w]+)/seasons/', include(season_urls, namespace='seasons')),
+    url(r'^(?P<slug>[-\w]+)/seasons/(?P<season_pk>\d+)/', include(season_urls, namespace='seasons')),
 ]
--- apps/seasons/urls.py
+++ apps/seasons/urls.py
@@ -4,11 +4,6 @@ from teams.views import TeamDetailSeasonRostersView
 from . import views
 
 
-season_urls = [
-    url(r'^(?P<season_pk>\d+)/$', views.SeasonDetailScheduleView.as_view(), name='schedule'),
-    url(r'^(?P<season_pk>\d+)/divisions/$', views.SeasonDetailDivisionsView.as_view(), name='divisions'),
-]
-
 season_roster_urls = [
     url(r'^$', TeamDetailSeasonRostersView.as_view(), name='list'),
     url(r'^create/$', views.SeasonRosterCreateView.as_view(), name='create'),
