2b0fe5154df71e56fbf354cd7279c71cf2e306b4
Harris Pittinsky <harris@pittinsky.com>
2019-02-10 22:08:34 -0500

    Use helper function to fetch player model class and player serializer class

--- apps/api/v1/players/views.py
+++ apps/api/v1/players/views.py
@@ -3,7 +3,7 @@ from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 
 from api.exceptions import SportNotConfiguredAPIException
-from ayrabo.utils.mappings import SPORT_PLAYER_MODEL_MAPPINGS, SPORT_PLAYER_SERIALIZER_MAPPINGS
+from players.mappings import get_player_model_cls, get_player_serializer_cls
 from teams.models import Team
 
 
@@ -21,14 +21,9 @@ class PlayersListAPIView(generics.ListAPIView):
         return self.team
 
     def get_serializer_class(self):
-        serializer_cls = SPORT_PLAYER_SERIALIZER_MAPPINGS.get(self.sport.name)
-        if serializer_cls is None:
-            raise SportNotConfiguredAPIException(self.sport)
-        return serializer_cls
+        return get_player_serializer_cls(self.sport, exception_cls=SportNotConfiguredAPIException)
 
     def get_queryset(self):
         team = self._get_team()
-        model_cls = SPORT_PLAYER_MODEL_MAPPINGS.get(self.sport.name)
-        if model_cls is None:
-            raise SportNotConfiguredAPIException(self.sport)
+        model_cls = get_player_model_cls(self.sport, exception_cls=SportNotConfiguredAPIException)
         return model_cls.objects.filter(team=team).select_related('user')
