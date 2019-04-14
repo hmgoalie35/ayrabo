7db005afe1e7d0a050cf03bf7ba4c55c27dc814c
Harris Pittinsky <harris@pittinsky.com>
2019-04-14 15:13:20 -0400

    Update .idea settings

--- apps/api/utils/serializer_fields.py
+++ apps/api/utils/serializer_fields.py
@@ -5,13 +5,17 @@ from rest_framework import serializers
 class EasyThumbnailField(serializers.ImageField):
     def __init__(self, *args, **kwargs):
         self.alias = kwargs.pop('alias', None)
-        assert self.alias is not None, 'alias is required'
+        assert self.alias is not None, 'The alias kwarg is required.'
         kwargs.update({
             'read_only': True
         })
         super().__init__(*args, **kwargs)
 
     def to_representation(self, value):
-        # TODO Error handling
-        thumb = get_thumbnailer(value)[self.alias]
+        thumbnailer = get_thumbnailer(value)
+        try:
+            # This can throw a `KeyError` if the alias hasn't been configured.
+            thumb = thumbnailer[self.alias]
+        except KeyError:
+            thumb = None
         return super().to_representation(thumb)
