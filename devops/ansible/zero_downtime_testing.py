929a0aad3cc1f7799bcd86ae49956e2a84d17c4d
Harris Pittinsky <harris@pittinsky.com>
2019-04-27 14:54:36 -0400

    Update script to handle multiple envs, show how long site down

--- devops/ansible/zero_downtime_testing.py
+++ devops/ansible/zero_downtime_testing.py
@@ -1,30 +1,55 @@
 #!/usr/bin/python3.6
-
+import argparse
 import time
+from datetime import datetime
+from urllib.parse import urljoin
 
 import requests
 
 
-SITE_URL = 'https://192.168.1.88/'
+def get_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('env', choices=('dev', 'qa', 'staging', 'prod'))
+    return parser
 
 
 def print_msg(msg):
-    print()
-    print('#' * 10)
-    print(msg)
-    print('#' * 10)
-    print()
+    print('[{}] {}'.format(datetime.now(), msg))
 
 
 def main():
+    parser = get_parser()
+    args = vars(parser.parse_args())
+    env = args.get('env')
+    base_urls = {
+        'dev': 'http://localhost:8000',
+        'qa': 'https://192.168.1.188',
+        'staging': None,
+        'prod': None
+    }
+    base_url = base_urls.get(env)
+    if not base_url:
+        print('{} not configured'.format(env))
+        exit(1)
+    url = urljoin(base_url, 'health-check')
+    downtime_start = None
+    downtime_end = None
+    session = requests.Session()
     while True:
-        resp = requests.get(SITE_URL, verify=False)
-        if resp.ok:
+        response = session.get(url, verify=False)
+        status_code = response.status_code
+        if response.ok:
             print_msg('SUCCESS')
-            time.sleep(3)
+            if downtime_start:
+                downtime_end = datetime.now()
+                break
+            time.sleep(1)
         else:
-            print_msg('ERROR: {} {}'.format(resp.status_code, resp.reason))
-            break
+            print_msg('ERROR {} {}'.format(status_code, response.reason))
+            if not downtime_start:
+                downtime_start = datetime.now()
+    if downtime_start and downtime_end:
+        print('Total downtime: {}'.format(downtime_end - downtime_start))
     print('Exiting...')
 
 
