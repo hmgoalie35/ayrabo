b4601035fbb3e18f46c1183e721601abbab944ea
Harris Pittinsky <harris@pittinsky.com>
2019-07-21 20:36:28 -0400

    Make instance arg optional for AbstractGameRosterSerializer

--- apps/api/v1/games/serializers.py
+++ apps/api/v1/games/serializers.py
@@ -9,22 +9,33 @@ class AbstractGameRosterSerializer(serializers.ModelSerializer):
     player_model_cls = None
     not_permitted_msg = 'You do not have permission to perform this action.'
 
-    def __init__(self, instance, *args, **kwargs):
+    def __init__(self, instance=None, *args, **kwargs):
         super().__init__(instance, *args, **kwargs)
         if self.player_model_cls is None:  # pragma: no cover
             raise exceptions.ImproperlyConfigured('You need to specify player_model_cls.')
-
-        home_team = instance.home_team
-        away_team = instance.away_team
 
         # This prevents players from other teams, divisions, leagues from being added to the roster. If this serializer
         # isn't used to add players, it is possible the player will be successfully added but the api endpoint won't
         # include that player.
         qs = self.player_model_cls.objects.active().select_related('user')
-        self.fields['home_players'] = serializers.PrimaryKeyRelatedField(many=True, queryset=qs.filter(team=home_team),
-                                                                         label='Home Roster')
-        self.fields['away_players'] = serializers.PrimaryKeyRelatedField(many=True, queryset=qs.filter(team=away_team),
-                                                                         label='Away Roster')
+        if instance:
+            home_players = qs.filter(team=instance.home_team)
+            away_players = qs.filter(team=instance.away_team)
+        else:
+            empty_qs = self.player_model_cls.objects.none()
+            home_players = empty_qs
+            away_players = empty_qs
+
+        self.fields['home_players'] = serializers.PrimaryKeyRelatedField(
+            many=True,
+            queryset=home_players,
+            label='Home Roster'
+        )
+        self.fields['away_players'] = serializers.PrimaryKeyRelatedField(
+            many=True,
+            queryset=away_players,
+            label='Away Roster'
+        )
 
     def validate_home_players(self, value):
         if not self.context.get('can_update_home_roster'):
