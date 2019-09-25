4981d3ae3f2ece5b37ec4d7b2dd7c57583d39758
Harris Pittinsky <harris@pittinsky.com>
2019-09-24 22:50:02 -0400

    Remove now unused get object and create object command utils

--- apps/common/management/commands/_utils.py
+++ apps/common/management/commands/_utils.py
@@ -1,27 +1,3 @@
-from django.core.exceptions import ValidationError
-
-
-def get_object(cls, **kwargs):
-    try:
-        return cls.objects.get(**kwargs)
-    except cls.DoesNotExist:
-        return None
-
-
-def create_object(cls, exclude=None, **kwargs):
-    exclude = exclude or []
-    created = False
-    try:
-        instance = cls(**kwargs)
-        instance.full_clean(exclude=exclude)
-        instance.save()
-        created = True
-    except ValidationError as e:
-        print(e)
-        instance = None
-    return instance, created
-
-
 def get_or_create(cls, get_kwargs, create_kwargs, exclude=None):
     exclude = exclude or []
     created = False
