e57824ba29f1f8849fe4cb87da93b8d890c587af
Harris Pittinsky <harris@pittinsky.com>
2019-10-17 22:58:49 -0400

    Update get past seasons nav tab url templatetag to account for all seasons

--- apps/common/templatetags/utils.py
+++ apps/common/templatetags/utils.py
@@ -50,13 +50,15 @@ def copy_to_clipboard_btn(text, title='Copy'):
 
 
 @register.simple_tag(takes_context=True)
-def get_past_seasons_nav_tab_url(context, profile_type):
-    past_season = context.get('past_season')
+def get_seasons_nav_tab_url(context, profile_type):
+    # Pulling `season` from context will give us the season the view chucks into the context which isn't what we want
+    # here, need to name it something unique
+    season = context.get('season_dropdown_obj')
     if profile_type == 'league':
         league = context.get('league')
-        return reverse('leagues:seasons:schedule', kwargs={'slug': league.slug, 'season_pk': past_season.pk})
+        return reverse('leagues:seasons:schedule', kwargs={'slug': league.slug, 'season_pk': season.pk})
     elif profile_type == 'team':
         team = context.get('team')
-        return reverse('teams:seasons:schedule', kwargs={'team_pk': team.pk, 'season_pk': past_season.pk})
+        return reverse('teams:seasons:schedule', kwargs={'team_pk': team.pk, 'season_pk': season.pk})
     # Don't return `None`, None will be used as the url.
     return ''
--- apps/common/tests/test_templatetags.py
+++ apps/common/tests/test_templatetags.py
@@ -1,7 +1,7 @@
 from django.urls import reverse
 
 from ayrabo.utils.testing import BaseTestCase
-from common.templatetags.utils import get_past_seasons_nav_tab_url
+from common.templatetags.utils import get_seasons_nav_tab_url
 from divisions.tests import DivisionFactory
 from leagues.tests import LeagueFactory
 from seasons.tests import SeasonFactory
@@ -14,23 +14,23 @@ class TemplateTagUtilsTests(BaseTestCase):
         self.ice_hockey = SportFactory(name='Ice Hockey')
         self.liahl = LeagueFactory(sport=self.ice_hockey, name='Long Island Amateur Hockey League')
         # Don't really care about the start/end dates here.
-        self.past_season = SeasonFactory(league=self.liahl)
+        self.season = SeasonFactory(league=self.liahl)
 
-    def test_get_past_seasons_nav_tab_url_league(self):
-        ctx = {'league': self.liahl, 'past_season': self.past_season}
-        url = get_past_seasons_nav_tab_url(ctx, 'league')
-        expected_url = reverse('leagues:seasons:schedule', kwargs={'slug': 'liahl', 'season_pk': self.past_season.pk})
+    def test_get_seasons_nav_tab_url_league(self):
+        ctx = {'league': self.liahl, 'season_dropdown_obj': self.season}
+        url = get_seasons_nav_tab_url(ctx, 'league')
+        expected_url = reverse('leagues:seasons:schedule', kwargs={'slug': 'liahl', 'season_pk': self.season.pk})
         self.assertEqual(url, expected_url)
 
-    def test_get_past_seasons_nav_tab_url_team(self):
+    def test_get_seasons_nav_tab_url_team(self):
         division = DivisionFactory(league=self.liahl, name='Midget Minor AA')
         team = TeamFactory(division=division)
-        ctx = {'league': self.liahl, 'past_season': self.past_season, 'team': team}
-        url = get_past_seasons_nav_tab_url(ctx, 'team')
-        expected_url = reverse('teams:seasons:schedule', kwargs={'team_pk': team.pk, 'season_pk': self.past_season.pk})
+        ctx = {'league': self.liahl, 'season_dropdown_obj': self.season, 'team': team}
+        url = get_seasons_nav_tab_url(ctx, 'team')
+        expected_url = reverse('teams:seasons:schedule', kwargs={'team_pk': team.pk, 'season_pk': self.season.pk})
         self.assertEqual(url, expected_url)
 
-    def test_get_past_seasons_nav_tab_url_none(self):
-        ctx = {'league': self.liahl, 'past_season': self.past_season}
-        url = get_past_seasons_nav_tab_url(ctx, 'invalid_profile_type')
+    def test_get_seasons_nav_tab_url_none(self):
+        ctx = {'league': self.liahl, 'season_dropdown_obj': self.season}
+        url = get_seasons_nav_tab_url(ctx, 'invalid_profile_type')
         self.assertEqual(url, '')
