e3fc50e3892a1ebcf8b7846b08175f6dd325dfc9
Harris Pittinsky <harris@pittinsky.com>
2019-10-19 23:36:08 -0400

    Add season manager func to get seasons for a league, update tests

--- apps/seasons/tests/test_managers.py
+++ apps/seasons/tests/test_managers.py
@@ -1,4 +1,4 @@
-from datetime import date, timedelta
+from datetime import datetime, timedelta
 from unittest import mock
 
 from ayrabo.utils.testing import BaseTestCase
@@ -11,15 +11,16 @@ from sports.tests import SportFactory
 class SeasonManagerTests(BaseTestCase):
     def setUp(self):
         self.ice_hockey = SportFactory(name='Ice Hockey')
-        self.liahl = LeagueFactory(name='Long Island Amateur Hockey League')
-        self.past_past_start_date = date(2015, 9, 4)
-        self.past_start_date = date(2016, 9, 4)
-        self.start_date = date(2017, 9, 4)
-        self.end_date = date(2018, 9, 4)
-        self.future_end_date = date(2019, 9, 4)
+        self.liahl = LeagueFactory(sport=self.ice_hockey, name='Long Island Amateur Hockey League')
+        self.past_past_start_date = datetime(2015, 9, 4)
+        self.past_start_date = datetime(2016, 9, 4)
+        self.start_date = datetime(2017, 9, 4)
+        self.end_date = datetime(2018, 9, 4)
+        self.future_end_date = datetime(2019, 9, 4)
         # 2015-2016 season
-        self.past_past_season = SeasonFactory(league=self.liahl, start_date=self.past_past_start_date,
-                                              end_date=self.past_start_date)
+        self.past_past_season = SeasonFactory(
+            league=self.liahl, start_date=self.past_past_start_date, end_date=self.past_start_date
+        )
         # 2016-2017 season
         self.past_season = SeasonFactory(league=self.liahl, start_date=self.past_start_date, end_date=self.start_date)
         # 2017-2018 season
@@ -34,38 +35,48 @@ class SeasonManagerTests(BaseTestCase):
         season = self._get_current_season()
         self.assertEqual(season.id, self.current_season.id)
 
-    @mock.patch('seasons.managers.date')
-    def test_get_current_today_before_season_start(self, mock_date):
-        mock_date.today.return_value = self.start_date - timedelta(days=60)
+    def test_get_for_league(self):
+        nhl = LeagueFactory(sport=self.ice_hockey, name='National Hockey League')
+        SeasonFactory(league=nhl)
+
+        seasons = Season.objects.get_for_league(league=self.liahl)
+        self.assertListEqual(
+            list(seasons),
+            [self.future_season, self.current_season, self.past_season, self.past_past_season]
+        )
+
+    @mock.patch('django.utils.timezone.now')
+    def test_get_current_today_before_season_start(self, mock_now):
+        mock_now.return_value = self.start_date - timedelta(days=60)
         self.assertEqual(self._get_current_season().id, self.past_season.id)
 
-    @mock.patch('seasons.managers.date')
-    def test_get_current_today_same_as_season_start(self, mock_date):
-        mock_date.today.return_value = self.start_date
+    @mock.patch('django.utils.timezone.now')
+    def test_get_current_today_same_as_season_start(self, mock_now):
+        mock_now.return_value = self.start_date
         self._assert_current_season()
 
-    @mock.patch('seasons.managers.date')
-    def test_get_current_today_after_season_start(self, mock_date):
-        mock_date.today.return_value = self.start_date + timedelta(days=60)
+    @mock.patch('django.utils.timezone.now')
+    def test_get_current_today_after_season_start(self, mock_now):
+        mock_now.return_value = self.start_date + timedelta(days=60)
         self._assert_current_season()
 
-    @mock.patch('seasons.managers.date')
-    def test_get_current_today_past_new_year(self, mock_date):
-        mock_date.today.return_value = date(2018, 1, 1)
+    @mock.patch('django.utils.timezone.now')
+    def test_get_current_today_past_new_year(self, mock_now):
+        mock_now.return_value = datetime(2018, 1, 1)
         self._assert_current_season()
 
-    @mock.patch('seasons.managers.date')
-    def test_get_current_today_same_as_season_end(self, mock_date):
-        mock_date.today.return_value = self.end_date
+    @mock.patch('django.utils.timezone.now')
+    def test_get_current_today_same_as_season_end(self, mock_now):
+        mock_now.return_value = self.end_date
         self.assertEqual(self._get_current_season().id, self.future_season.id)
 
-    @mock.patch('seasons.managers.date')
-    def test_get_current_today_after_season_end(self, mock_date):
-        mock_date.today.return_value = self.end_date + timedelta(days=60)
+    @mock.patch('django.utils.timezone.now')
+    def test_get_current_today_after_season_end(self, mock_now):
+        mock_now.return_value = self.end_date + timedelta(days=60)
         self.assertEqual(self._get_current_season().id, self.future_season.id)
 
-    @mock.patch('seasons.managers.date')
-    def test_get_past(self, mock_date):
-        mock_date.today.return_value = self.end_date
+    @mock.patch('django.utils.timezone.now')
+    def test_get_past(self, mock_now):
+        mock_now.return_value = self.end_date
         past_seasons = Season.objects.get_past(league=self.liahl)
         self.assertListEqual(list(past_seasons), [self.past_season, self.past_past_season])
