d6f318f1277b1bdb6724f45340161dd22e0bdbfc
Harris Pittinsky <harris@pittinsky.com>
2020-03-04 21:29:39 -0500

    Delete overridden admin change list templates, make admin bulk upload template more generic, update hockey game sample csv to have team, delete unnecessary forms, admin files

--- ayrabo/utils/admin.py
+++
@@ -1,8 +0,0 @@
-from django.utils.html import format_html
-
-
-def format_website_link(obj, attr='website'):
-    url = getattr(obj, attr, None)
-    if url:
-        return format_html('<a target="_blank" rel="noopener noreferrer" href="{url}">{url}</a>', url=url)
-    return None
