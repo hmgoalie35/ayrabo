c49685950595e7144110e74f97c4fd1d85f0f33b
Harris Pittinsky <harris@pittinsky.com>
2020-03-14 18:53:37 -0400

    Add bulk upload user feature

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,9 +1,46 @@
+from allauth.account.models import EmailAddress
+from django import forms
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.forms import BaseModelFormSet
 
+from ayrabo.utils.admin.mixins import AdminBulkUploadMixin
 from .models import Permission, User
 
-admin.site.register(User, UserAdmin)
+
+class UserAdminForm(forms.ModelForm):
+    # max_length taken from AbstractBaseUser
+    username = forms.CharField(max_length=150, required=False)
+
+    def clean(self):
+        super().clean()
+        email = self.cleaned_data.get('email')
+        self.cleaned_data.update({'username': email})
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'first_name', 'last_name')
+
+
+class UserAdminModelFormSet(BaseModelFormSet):
+    def save(self, commit=True):
+        instances = super().save(commit=commit)
+        for user in instances:
+            email_address = EmailAddress.objects.create(
+                user=user,
+                email=user.email,
+                primary=True,
+                verified=False
+            )
+            email_address.send_confirmation()
+        return instances
+
+
+@admin.register(User)
+class UserAdmin(AdminBulkUploadMixin, BaseUserAdmin):
+    bulk_upload_sample_csv = 'bulk_upload_users_example.csv'
+    bulk_upload_model_form_class = UserAdminForm
+    bulk_upload_model_formset_class = UserAdminModelFormSet
 
 
 @admin.register(Permission)
