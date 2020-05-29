365377852319238dcb472c7ce0e6e829c86606db
Harris Pittinsky <harris@pittinsky.com>
2020-05-29 00:37:08 -0400

    Use game authorizer in game roster update api view and permission

--- apps/api/v1/games/permissions.py
+++ apps/api/v1/games/permissions.py
@@ -1,15 +1,11 @@
-from django.db.models import Q
 from rest_framework import permissions
-
-from managers.models import Manager
-from scorekeepers.models import Scorekeeper
 
 
 class GameRostersRetrieveUpdatePermission(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
-        user = request.user
-        sport = obj.team.division.league.sport
-        managers = Manager.objects.active().filter(Q(team=obj.home_team) | Q(team=obj.away_team), user=user)
-        scorekeepers = Scorekeeper.objects.active().filter(user=user, sport=sport)
-        return managers.exists() or scorekeepers.exists()
+        return view.game_authorizer.can_user_update_game_rosters(
+            home_team=obj.home_team,
+            away_team=obj.away_team,
+            sport=view.sport
+        )
