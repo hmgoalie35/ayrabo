9d0dadab7bdfda32bad92207095c05b960af4645
Harris Pittinsky <harris@pittinsky.com>
2020-06-01 21:45:41 -0400

    Fetch season in get object, if no season render misconfiguration page, add util to send admins email about misconfigured seasons

--- apps/teams/utils.py
+++ apps/teams/utils.py
@@ -1,7 +1,6 @@
 from django.urls import reverse
 
 from seasons.models import Season
-from seasons.utils import get_current_season_or_from_pk
 
 
 def get_team_detail_schedule_url(team, season):
@@ -24,10 +23,9 @@ def get_team_detail_season_rosters_url(team, season):
     return reverse('teams:seasons:season_rosters-list', kwargs=kwargs)
 
 
-def get_team_detail_view_context(team, season_pk=None):
+def get_team_detail_view_context(team, season=None):
     division = team.division
     league = division.league
-    season = get_current_season_or_from_pk(league, season_pk)
     return {
         'team_display_name': f'{team.name} - {division.name}',
         'season': season,  # Note `get_game_list_view_context` is also setting the same context key.
