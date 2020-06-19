9672c70dad495ca049e0344f61404c449b872933
Harris Pittinsky <harris@pittinsky.com>
2020-06-18 22:24:12 -0400

    Use better file name for alternative game urls

--- apps/sports/urls.py
+++ apps/sports/urls.py
@@ -8,7 +8,7 @@ app_name = 'sports'
 urlpatterns = [
     url(r'^register/$', views.SportRegistrationCreateView.as_view(), name='register'),
     url(r'^(?P<slug>[-\w]+)/dashboard/$', views.SportDashboardView.as_view(), name='dashboard'),
-    url(r'^(?P<slug>[-\w]+)/games/', include('games.roster_urls')),
+    url(r'^(?P<slug>[-\w]+)/games/', include('games.nested_sport_urls')),
     url(r'^(?P<slug>[-\w]+)/players/', include('players.urls')),
     url(r'^(?P<slug>[-\w]+)/coaches/', include('coaches.urls')),
 ]
