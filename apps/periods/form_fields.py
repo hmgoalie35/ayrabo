70f460e150664c0032309e86cd14ec24590222e8
Harris Pittinsky <harris@pittinsky.com>
2021-01-19 22:44:50 -0500

    Add period duration model field tests

--- apps/periods/form_fields.py
+++ apps/periods/form_fields.py
@@ -5,7 +5,7 @@ class PeriodDurationField(forms.IntegerField):
     def __init__(self, *args, **kwargs):
         from periods.model_fields import PERIOD_DURATION_MAX, PERIOD_DURATION_MIN
         kwargs.update({
-            'max_value': PERIOD_DURATION_MAX,
             'min_value': PERIOD_DURATION_MIN,
+            'max_value': PERIOD_DURATION_MAX,
         })
         super().__init__(*args, **kwargs)
